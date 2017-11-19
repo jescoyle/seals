98f894999a4e3f87d02846a31b426c849a86a1dc
Jes Coyle <jes.r.coyle@gmail.com>
2017-11-18 18:11:14 -0800

    finished app

--- seals_pop/app.R
+++ seals_pop/app.R
@@ -14,12 +14,12 @@ ui <- fluidPage(
    
    # Application title
    titlePanel("San Miguel Island Northern Elephant Seal Population Dynamics"),
-   
+
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
       sidebarPanel(
          numericInput("rmax",
-                     expression(r_max),
+                     HTML("r<sub>max</sub>"),
                      value = NA),
          numericInput("K",
                       "K",
@@ -28,7 +28,10 @@ ui <- fluidPage(
       
       # Show a plot of the data with the logistic curve
       mainPanel(
-         plotOutput("overlayPlot")
+        p("These data come from the article:"),
+        p("Lowry et al. 2014. Abundance, Distribution, and Population Growth of the Northern Elephant Seal (Mirounga angustirostris) in the United States from 1991 to 2010. Aquatic Mammals 40: 20-31, DOI: ", a("10.1578/AM.40.1.2014.20", href = "http://dx.doi.org/10.1578/AM.40.1.2014.20")),
+        p("The population breeding at San Miguel Island in the Channel Islands, California is estimated from the number of pups born each year using parameters provided in the paper."),
+        plotOutput("overlayPlot")
       )
    )
 )
@@ -37,18 +40,31 @@ ui <- fluidPage(
 server <- function(input, output) {
   
    output$overlayPlot <- renderPlot({
-      
      
+     # Make plot
+     plot(I(Population/1000) ~ Year, data = nes, type = 'n', las = 1, 
+          ylab = "Est. Population (thousands)")
      
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+     # Add gridlines
+     #abline(v = seq(1958, 2010, 2), col = 'grey')
+     #abline(h = seq(0, 70, 2), col = 'grey')
+     #abline(v = seq(1960, 2010, 10), col = 'grey20')
+     #abline(h = seq(0, 70, 10), col = 'grey20')
+     
+     # Add points
+     points(I(Population/1000) ~ Year, data = nes, type = 'o', pch = 16)
+     
+     my_curve <- function(x) logistic_growth(x - 1984, 
+                                             r = input$rmax, 
+                                             K = input$K, 
+                                             N0 = nes[nes$Year == 1984, 'Population']
+                                             ) / 1000 # divide by 1000 to put on same scale as plot
+     
+     curve(my_curve, from = 1958, to = 2010, add = T, col = 'orange', lwd=3)
+     
+  })
+   
 }
-
 
 # A logistic curve function
 logistic_growth <- function(x, r, K, N0){
@@ -58,10 +74,8 @@ logistic_growth <- function(x, r, K, N0){
 }
 
 # Read data from GitHub
-
-
-# Add column for annual avg rate of increase (from paper)
-nes$lambda <- with(nes, ifelse(Year < 1980, 1.197, ifelse(Year < 1991, 1.067, 1.005)))
+repo_url <- "https://raw.githubusercontent.com/jescoyle/seals/master/"
+nes <- read.csv(file.path(repo_url, "data/san_miguel_seal_births_Lowry_et_al_2014.csv"))
 
 # Add column for multipler to derive total pop size
 nes$multiplier <- with(nes, ifelse(Year < 1991, 4.4, 4.31))
